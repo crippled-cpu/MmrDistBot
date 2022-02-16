cfbb6d08831014b899a57b9733d71a888d9d0e4d
Daniel <56933050+crippled-cpu@users.noreply.github.com>
2022-02-15 23:43:55 -0800

    Add files via upload

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  GuildId: String,
+  UserId: String,
+  inGame: { type: Boolean, default: false },
+  inGameUsers: { type: Array, default: [] },
+  mmrTotal: { type: Number, default: 100.0 },
+  mmrPlayed: { type: Number, default: 0 },
+  mmrWon: { type: Number, default: 0 },
+  mmrLost: { type: Number, default: 0 },
+  mmrWinStreak: { type: Number, default: 0 },
+  mmrLoseStreak: { type: Number, default: 0 },
+  mmrLastPlayed: { type: Number, default: 0 },
+  mmrHighestWinStreak: { type: Number, default: 0 },
+  mmrHighestLoseStreak: { type: Number, default: 0 },
+});
 
+module.exports = mongoose.model("mmr-users", userSchema);
